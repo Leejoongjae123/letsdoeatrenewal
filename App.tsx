0c42d1f31a782fbf1c272c485b83b07a2e0f8741
안세영 <ayh747474@naver.com>
2024-03-18 22:38:19 +0900

    [Add] splash-screen 적용

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { ThemeProvider } from "react-native-rapi-ui";
-import Navigation from "./src/navigation";
-import { AuthProvider } from "./src/provider/AuthProvider";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ThemeProvider } from 'react-native-rapi-ui';
+import Navigation from './src/navigation';
+import { AuthProvider } from './src/provider/AuthProvider';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
-  const images = [
-    require("./assets/images/login.png"),
-    require("./assets/images/register.png"),
-    require("./assets/images/forget.png"),
-  ];
-  return (
-    <ThemeProvider images={images}>
-      <AuthProvider>
-        <Navigation />
-      </AuthProvider>
-      <StatusBar />
-    </ThemeProvider>
-  );
+    SplashScreen.preventAutoHideAsync();
+    const images = [require('./assets/images/login.png'), require('./assets/images/register.png'), require('./assets/images/forget.png')];
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            SplashScreen.hideAsync();
+        }, 3000);
+    }, []);
+
+    return (
+        <ThemeProvider images={images}>
+            <AuthProvider>
+                <Navigation />
+            </AuthProvider>
+            <StatusBar />
+        </ThemeProvider>
+    );
 }
