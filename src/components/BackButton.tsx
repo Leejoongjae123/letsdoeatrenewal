24c88bcb2e7f952c0dfe4b9ed44f92f6403d6701
안세영 <ayh747474@naver.com>
2024-03-19 01:18:41 +0900

    [Add] BackButton component

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { getWidth } from '../lib/CrossDevice';
+
+type PropsType = {
+    onPress: () => void;
+    white?: boolean;
+};
+
+const BackButton = ({ onPress, white }: PropsType) => {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.container}>
+            {white ? (
+                <Image style={{ width: getWidth(22), height: getWidth(22) }} source={require('../../assets/images/common/Icon_arrow_left_white.png')} />
+            ) : (
+                <Image style={{ width: getWidth(22), height: getWidth(22) }} source={require('../../assets/images/common/Icon_arrow_left.png')} />
+            )}
+        </TouchableOpacity>
+    );
+};
+
+export default BackButton;
+
+const styles = StyleSheet.create({
+    container: {
+        width: 28,
+        height: 28,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
