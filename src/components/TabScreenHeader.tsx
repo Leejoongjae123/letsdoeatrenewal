4c2347b18a7f6b57f8bf8bbd0b47889e7de32972
안세영 <ayh747474@naver.com>
2024-03-21 20:01:43 +0900

    [Add] TabScreen 헤더 컴포넌트 추가

---
+++ src/components/TabScreenHeader.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { Text, View, StatusBar, StyleSheet, Platform } from 'react-native';
+import { getFont, getHeight, getWidth } from '../lib/CrossDevice';
+import { BrandColor, DefaultBlack, WhiteColor } from '../utils/Colors';
+
+interface Props {
+    headerLeft?: React.ReactNode;
+    headerTitle?: React.ReactNode | string;
+    headerRight?: React.ReactNode;
+    underline?: boolean;
+    transparentStatus?: boolean;
+}
+
+const TabScreenHeader = ({ headerLeft, headerTitle, headerRight, underline, transparentStatus }: Props) => {
+    return (
+        <View
+            style={{
+                ...styles.container,
+                borderBottomWidth: underline ? 1 : 0,
+            }}
+        >
+            <View style={styles.leftContainer}>{typeof headerTitle === 'string' ? <Text style={styles.titleText}>{headerTitle}</Text> : headerTitle}</View>
+            <View style={styles.centerContainer}></View>
+            <View style={styles.rightContainer}>{headerRight}</View>
+        </View>
+    );
+};
+
+export default TabScreenHeader;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: getHeight(56),
+        paddingHorizontal: getWidth(20),
+        backgroundColor: BrandColor,
+
+        marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    },
+    leftContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    centerContainer: {
+        flex: 2,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    titleText: {
+        fontSize: getFont(32),
+        textAlign: 'left',
+        color: DefaultBlack,
+        fontFamily: 'Bold',
+    },
+    rightContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
+});
