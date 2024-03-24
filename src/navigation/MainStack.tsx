aec93b7b7f4bd69cdc9da8af43bb0987530a27b0
안세영 <ayh747474@naver.com>
2024-03-24 23:57:05 +0900

    [Update] MainStack, MainTabs navigation 설정 업데이트

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,21 +1,55 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import SecondScreen from "../screens/SecondScreen";
-import MainTabs from "./MainTabs";
+import MainTabs from './MainTabs';
+import AlarmList from '../screens/AlarmList';
+
+// 마이페이지 스크린
+import Reward from '../screens/mypage/Reward';
+import Withraw from '../screens/mypage/Withraw';
+import ProgressContest from '../screens/mypage/ProgressContest';
+import CompleteContest from '../screens/mypage/CompleteContest';
+import Notice from '../screens/mypage/Notice';
+import Check from '../screens/mypage/Check';
+import BankVerify from '../screens/mypage/BankVerify';
+// 마이페이지 스크린
+
+// 카메라
+import CameraScreen from '../screens/camera/Camera';
+import CameraEditScreen from '../screens/camera/CameraEdit';
+import CameraSelectScreen from '../screens/camera/CameraSelect';
+import CameraStorageScreen from '../screens/camera/CameraStorage';
+import CameraUploadScreen from '../screens/camera/CameraUpload';
+// 카메라
 
 const MainStack = createNativeStackNavigator();
 const Main = () => {
-  return (
-    <MainStack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <MainStack.Screen name="MainTabs" component={MainTabs} />
-      <MainStack.Screen name="SecondScreen" component={SecondScreen} />
-    </MainStack.Navigator>
-  );
+    return (
+        <MainStack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <MainStack.Screen name="MainTabs" component={MainTabs} />
+            <MainStack.Screen name="AlarmList" component={AlarmList} />
+            {/* 마이페이지 스크린 */}
+            <MainStack.Screen name="Reward" component={Reward} />
+            <MainStack.Screen name="Withraw" component={Withraw} />
+            <MainStack.Screen name="ProgressContest" component={ProgressContest} />
+            <MainStack.Screen name="CompleteContest" component={CompleteContest} />
+            <MainStack.Screen name="Notice" component={Notice} />
+            <MainStack.Screen name="Check" component={Check} />
+            <MainStack.Screen name="BankVerify" component={BankVerify} />
+            {/* 마이페이지 스크린 */}
+            {/* 카메라 */}
+            <MainStack.Screen name="Camera" component={CameraScreen} />
+            <MainStack.Screen name="CameraEdit" component={CameraEditScreen} />
+            <MainStack.Screen name="CameraSelect" component={CameraSelectScreen} />
+            <MainStack.Screen name="CameraStorage" component={CameraStorageScreen} />
+            <MainStack.Screen name="CameraUpload" component={CameraUploadScreen} />
+            {/* 카메라 */}
+        </MainStack.Navigator>
+    );
 };
 
 export default Main;
--- src/navigation/MainTabs.tsx
+++ src/navigation/MainTabs.tsx
@@ -1,66 +1,39 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { themeColor, useTheme } from "react-native-rapi-ui";
-import TabBarIcon from "../components/utils/TabBarIcon";
-import TabBarText from "../components/utils/TabBarText";
+import { themeColor, useTheme } from 'react-native-rapi-ui';
+import TabBarIcon from '../components/utils/TabBarIcon';
+import TabBarText from '../components/utils/TabBarText';
 
-import Home from "../screens/Home";
-import About from "../screens/About";
-import Profile from "../screens/Profile";
+import Home from '../screens/Home';
+
+import CustomTabBar from '../components/CustomTabBar';
+import Contest from '../screens/Contest';
+import Shop from '../screens/Shop';
+import Mypage from '../screens/Mypage';
+import ContestDetail from '../screens/contest/ContestDetail';
+import Profile from '../screens/mypage/Profile';
+import ProfileEdit from '../screens/mypage/ProfileEdit';
 
 const Tabs = createBottomTabNavigator();
 const MainTabs = () => {
-  const { isDarkmode } = useTheme();
-  return (
-    <Tabs.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          borderTopColor: isDarkmode ? themeColor.dark100 : "#c0c0c0",
-          backgroundColor: isDarkmode ? themeColor.dark200 : "#ffffff",
-        },
-      }}
-    >
-      {/* these icons using Ionicons */}
-      <Tabs.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarLabel: ({ focused }) => (
-            <TabBarText focused={focused} title="Home" />
-          ),
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon focused={focused} icon={"md-home"} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          tabBarLabel: ({ focused }) => (
-            <TabBarText focused={focused} title="Profile" />
-          ),
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon focused={focused} icon={"person"} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="About"
-        component={About}
-        options={{
-          tabBarLabel: ({ focused }) => (
-            <TabBarText focused={focused} title="About" />
-          ),
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon focused={focused} icon={"ios-information-circle"} />
-          ),
-        }}
-      />
-    </Tabs.Navigator>
-  );
+    const { isDarkmode } = useTheme();
+    return (
+        <Tabs.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+            tabBar={(props) => <CustomTabBar {...props} />}
+        >
+            <Tabs.Screen name="Home" component={Home} />
+            <Tabs.Screen name="Contest" component={Contest} />
+            <Tabs.Screen name="Shop" component={Shop} />
+            <Tabs.Screen name="Mypage" component={Mypage} />
+            <Tabs.Screen name="ContestDetail" component={ContestDetail} />
+            <Tabs.Screen name="Profile" component={Profile} />
+            <Tabs.Screen name="ProfileEdit" component={ProfileEdit} />
+        </Tabs.Navigator>
+    );
 };
 
 export default MainTabs;
