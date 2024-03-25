3af2d9a5e7fbde945f40ecfc02d91fca5bdc8fd6
안세영 <ayh747474@naver.com>
2024-03-25 22:59:30 +0900

    [Fix] CrossDevice - 안드로이드 getHeight 함수 status bar 높이값으로 인해 조금 작게 나타나는부분 수정

--- src/lib/CrossDevice.ts
+++ src/lib/CrossDevice.ts
@@ -1,4 +1,4 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform, StatusBar } from 'react-native';
 const FIGMA_WINDOW_WIDTH = 375;
 const FIGMA_WINDOW_HEIGHT = 812;
 const portrait = Dimensions.get('window').width < Dimensions.get('window').height;
@@ -10,11 +10,16 @@ export function getWidth(width: number) {
 }
 
 export function getHeight(height: number) {
+    const statusHeight = StatusBar.currentHeight || 0;
     const percentage = portrait ? (height / FIGMA_WINDOW_HEIGHT) * 100 : (height / FIGMA_WINDOW_WIDTH) * 100;
+
+    if (Platform.OS === 'android') {
+        return ((Dimensions.get('window').height + statusHeight * 3.5) / 100) * percentage;
+    }
     return (Dimensions.get('window').height / 100) * percentage;
 }
 
 export function getFont(size: number) {
     const percentage = portrait ? (size / FIGMA_WINDOW_WIDTH) * 100 : (size / FIGMA_WINDOW_HEIGHT) * 100;
-    return (Dimensions.get('window').width / 100) * percentage > size ? size : (Dimensions.get('window').width / 100) * percentage;
+    return (Dimensions.get('window').width / 100) * percentage;
 }
